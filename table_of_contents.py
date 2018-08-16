556716bb91fd86e9507a4df6bbdd6be8a75a9f76
Trontor <Trontor@users.noreply.github.com>
2018-08-17 05:31:57 +1000

    Added index link to TOC

--- table_of_contents.py
+++ table_of_contents.py
@@ -1,5 +1,7 @@
 import re
 
+def cleanse(str):
+    return str.replace("#", "").strip().lower().replace(" ","-")
 with open("notes.md", encoding="utf-8") as file:
     text = file.read()
     with open("Source\MD Rendering\\notes.bak", 'w', encoding="utf-8")  as backup_file:
@@ -8,6 +10,7 @@ TOC_append = "# Table of Contents\n"
 if TOC_append in text:
     text = text.replace(TOC_append, "")
     print("Removed existing TOC")
+# Oh, scary regex!
 regex_op = r"(^# +.*$)"
 match_obj = re.findall(regex_op, text, re.MULTILINE)
 if match_obj is None or len(match_obj) ==0:
@@ -17,12 +20,21 @@ first_header = match_obj[0]
 print("Identified first header: %s" % first_header)
 remove_toc = text.split(first_header)[0]
 text = text.replace(remove_toc, "")
+
+for str in match_obj[1:]:
+    new_str = "%s\n[← Return to Index](#%s)" % (str, cleanse(TOC_append))
+    if not new_str in text:
+        text = text.replace(str, new_str)
+        print(new_str)
+    else:
+        print("There is already an index specifier for %s"% str)
+
 list_h1 = []
 new_line_split = text.split("\n")
 # Store a list of headers in order
 header_dict = {}
 for str in new_line_split:
-    cleansed = str.replace("#", "").strip().lower().replace(" ","-")
+    cleansed = cleanse(str)
     if str.startswith("# ") or str.startswith("## "):
         header_dict[str] = cleansed
 for key,val in header_dict.items():
@@ -31,5 +43,7 @@ for key,val in header_dict.items():
     TOC_append += "\t" * tab_count + output_str + "\n"
 text = TOC_append + text
 
-with open("notes.md",'w', encoding="utf-8") as output_file:
-     output_file.write(text)
+write_conf = input("Write to file?")
+if write_conf.lower().startswith("y"):
+    with open("notes.md",'w', encoding="utf-8") as output_file:
+         output_file.write(text)
