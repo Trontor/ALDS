7b1ed6d4a012dfcae5e6c678972d0f8271ed1e9c
Trontor <Trontor@users.noreply.github.com>
2018-08-17 05:57:03 +1000

    Added TOC to README.md

--- table_of_contents.py
+++ table_of_contents.py
@@ -21,6 +21,8 @@ print("Identified first header: %s" % first_header)
 remove_toc = text.split(first_header)[0]
 text = text.replace(remove_toc, "")
 
+readme_toc = TOC_append;
+
 for str in match_obj[:]:
     new_str = "%s\n[← Return to Index](#%s)" % (str, cleanse(TOC_append))
     if not new_str in text:
@@ -40,11 +42,24 @@ for str in new_line_split:
 
 for key,val in header_dict.items():
     output_str = "* [%s](#%s)" % (key.replace("#", "").strip(), val)
+    readme_str = "* [%s](notes.md#%s)" % (key.replace("#", "").strip(), val)
     tab_count = key.count("#") - 1
     TOC_append += "\t" * tab_count + output_str + "\n"
+    readme_toc += "\t" * tab_count + readme_str + "\n"
 text = TOC_append + text
 
-write_conf = input("Write to file?")
+write_conf = input("Write to notes.md file?")
 if write_conf.lower().startswith("y"):
     with open("notes.md",'w', encoding="utf-8") as output_file:
          output_file.write(text)
+
+if input("Update README.md? ").strip().lower().startswith("y"):
+    with open("readme.md", 'r+', encoding="utf-8") as readme_file:
+        existing_text = readme_file.read()
+        if "# Table of Contents" in existing_text:
+            print("TOC Already Exists in README.md")
+            existing_text = existing_text.split("# Table of Contents")[0].strip()
+        updated_text = existing_text + "\n" + readme_toc
+        readme_file.seek(0)
+        readme_file.truncate()
+        readme_file.write(updated_text)
